869982976351a6a069911a9efe1767673fcb3d9c
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-26 08:20:07 -0300

    Traballhando na parte do users

---
+++ theme/users/includes/head.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <title>System-Estans</title>
+
+    <!-- Fontfaces CSS-->
+    <link
+      href="../assets/style/theme/font-face.css"
+      rel="stylesheet"
+      media="all"
+    />
+    <link
+      href="../assets/fonts/font-awesome-5/css/fontawesome-all.min.css"
+      rel="stylesheet"
+      media="all"
+    />
+    <link
+      href="../assets/vendor/mdi-font/css/material-design-iconic-font.min.css"
+      rel="stylesheet"
+      media="all"
+    />
+
+    <!-- Bootstrap CSS-->
+    <link
+      href="../assets/vendor/bootstrap-4.1/bootstrap.min.css"
+      rel="stylesheet"
+      media="all"
+    />
+
+    <!-- Vendor CSS-->
+    <link
+      href="../assets/vendor/animsition/animsition.min.css"
+      rel="stylesheet"
+      media="all"
+    />
+
+    <!-- Main CSS-->
+    <link
+      href="../assets/styles/theme/theme.css"
+      rel="stylesheet"
+      media="all"
+    />
+
+    <!-- Animações -->
+    <link rel="stylesheet" href="../assets/styles/theme/aos.css" />
+
+    <link rel="stylesheet" href="../assets/styles/dashboard.css" />
+
+    <style>
+      .navbar-sidebar a {
+        color: #fff !important;
+      }
+    </style>
+  </head>
