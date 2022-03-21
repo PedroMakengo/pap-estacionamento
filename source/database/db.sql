a2aa135f60fd1cda5a9faada2759c3bf221a1145
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-03-21 12:52:47 -0300

    Trabalhando na parte do banco de dados

--- source/database/db.sql
+++ source/database/db.sql
@@ -1,11 +1,33 @@
 
-CREATE TABLE tb_admin();
+CREATE TABLE tb_admin(
+  id_admin int(11) PRIMARY KEY AUTO_INCREMENT
+  nome varchar(50),
+  email varchar(50),
+  senha varchar(50),
+  foto varchar(250)
+);
 
-CREATE TABLE tb_vaga();
+CREATE TABLE tb_vaga(
+  id_vaga int(11) PRIMARY KEY AUTO_INCREMENT, 
+  id_admin int(11),
+  vaga varchar(50)
+);
 
-CREATE TABLE tb_entrada_saida();
+CREATE TABLE tb_entrada_saida(
 
-CREATE TABLE tb_cliente();
+);
+
+CREATE TABLE tb_cliente(
+  id_cliente int(11) PRIMARY KEY AUTO_INCREMENT, 
+  nome_cliente varchar(50),
+  senha_cliente varchar(50),
+  foto_cliente varchar(250),
+  morada_cliente varchar(50)
+);
+
+CREATE TABLE tb_carro_cliente (
+  id_carro int(11), 
+)
 
 CREATE TABLE tb_estacionamento();
 
